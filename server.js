ad852ec0bb5ec83ce81f6d89623490437b6c40b7
mor bargig <morbargig@gmail.com>
2019-11-28 09:36:04 +0200

    heroku try

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
-const express = require('express');
 const path = require('path');
+const express = require('express');
 const app = express();
 
-// Serve static files....
-app.use(express.static(__dirname + '/dist/angular-wather-app'));
+// Serve static files
+app.use(express.static(__dirname + '/dist/MY_APP_NAME'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/angular-wather-app/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/MY_APP_NAME/index.html'));
 });
 
-// default Heroku PORT
-app.listen(process.env.PORT || 3000);
+// default Heroku port
+app.listen(process.env.PORT || 5000);
+
+// Heroku automagically gives us SSL
+// Lets write some middleware to redirect us
+let env = process.env.NODE_ENV || 'development';
+
+let forceSSL = (req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+  return next();
+};
+
+if (env === 'production') {
+  app.use(forceSSL);
+}
